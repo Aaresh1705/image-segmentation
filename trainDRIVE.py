a9dbd787b9c2e35ca701a0741b7879054bcffdd4
Marcus Olssen <s234816@dtu.dk>
2025-11-05 15:59:12 +0100

    feat: Updated training loop to loop over loss functions and also print validation loss

--- trainDRIVE.py
+++ trainDRIVE.py
@@ -1,53 +1,17 @@
 import torch
 import torch.optim as optim
-from time import time
+from torchsummary import summary
 
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 from lib.dataset import datasetDRIVE
 from lib.model import EncDecNet, DilatedNet, UNet
-from lib.losses import BCELoss, DiceLoss, FocalLoss, BCELoss_TotalVariation
+from lib import all_losses
 
-# Dataset
-size = 128
-transform = A.Compose([A.Resize(size, size),
-                        A.Normalize(mean=(0.0, 0.0, 0.0), std=(1.0, 1.0, 1.0)),  # <- converts to float32 for albumentations
-                        ToTensorV2(),
-                        ])
-  # transforms.Compose([transforms.Resize((size, size)), transforms.ToTensor()]))
 
-batch_size = 6
-(train_loader, val_loader, test_loader), (trainset, valset, testset) = datasetDRIVE(batch_size=batch_size, transform=transform)
-
-print(f"Loaded {len(trainset)} training images")
-print(f"Loaded {len(valset)} val images")
-print(f"Loaded {len(testset)} test images")
-
-# Training setup
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = EncDecNet().to(device)
-#model = UNet().to(device) # TODO
-#model = UNet2().to(device) # TODO
-#model = DilatedNet().to(device) # TODO
-#summary(model, (3, 256, 256))
-learning_rate = 0.001
-opt = optim.Adam(model.parameters(), learning_rate)
-
-loss_fn = BCELoss()
-#loss_fn = DiceLoss() # TODO
-#loss_fn = FocalLoss() # TODO
-#loss_fn = BCELoss_TotalVariation() # TODO
-epochs = 20
-
-# Training loop
-X_test, Y_test = next(iter(test_loader))
-
-model.train()  # train mode
-for epoch in range(epochs):
-    tic = time()
-    print(f'* Epoch {epoch+1}/{epochs}')
-
+def train(model, train_loader, loss_fn, opt):
+    model.train()  # train mode
     avg_loss = 0
     for X_batch, y_true in train_loader:
         X_batch = X_batch.to(device)
@@ -67,12 +31,62 @@ for epoch in range(epochs):
         # calculate metrics to show the user
         avg_loss += loss / len(train_loader)
 
+    model.eval()
+    avg_loss_val = 0
+    for X_val, y_val in val_loader:
+        X_val = X_val.to(device)
+        y_val = y_val.to(device)
+
+        y_pred = model(X_val)
+        loss = loss_fn(y_pred, y_val)
+        avg_loss_val += loss / len(val_loader)
+
     # IMPORTANT NOTE: It is a good practice to check performance on a
     # validation set after each epoch.
-    #model.eval()  # testing mode
-    #Y_hat = F.sigmoid(model(X_test.to(device))).detach().cpu()
-    print(f' - loss: {avg_loss}')
+    # model.eval()  # testing mode
+    # Y_hat = F.sigmoid(model(X_test.to(device))).detach().cpu()
+    print(f' -  train loss: {avg_loss:.3f}')
+    print(f' -  val loss:   {avg_loss_val:.3f}')
 
-# Save the model
-#torch.save(model, ....)
-print("Training has finished!")
+
+
+
+if __name__ == "__main__":
+    # Dataset
+    size = 128
+    transform = A.Compose([A.Resize(size, size),
+                           A.Normalize(mean=(0.0, 0.0, 0.0), std=(1.0, 1.0, 1.0)),
+                           # <- converts to float32 for albumentations
+                           ToTensorV2(),
+                           ])
+    # transforms.Compose([transforms.Resize((size, size)), transforms.ToTensor()]))
+
+    batch_size = 6
+    (train_loader, val_loader, test_loader), (trainset, valset, testset) = datasetDRIVE(batch_size=batch_size,
+                                                                                        transform=transform)
+
+    print(f"Loaded {len(trainset)} training images")
+    print(f"Loaded {len(valset)} val images")
+    print(f"Loaded {len(testset)} test images")
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = EncDecNet().to(device)
+    # model = UNet().to(device) # TODO
+    # model = UNet2().to(device) # TODO
+    # model = DilatedNet().to(device) # TODO
+    summary(model, (3, 256, 256))
+
+    learning_rate = 0.001
+    epochs = 20
+
+    for loss_function in all_losses:
+        opt = optim.Adam(model.parameters(), learning_rate)
+        loss_function = loss_function()
+
+        for epoch in range(epochs):
+            print(f'------==={{{epoch+1:>2}}}===------')
+            train(model, train_loader, loss_function, opt)
+
+    # Save the model
+    # torch.save(model, ....)
+    print("Training has finished!")
